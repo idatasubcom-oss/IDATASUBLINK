17807a51db546c6c36d8ec6f9c8958a938b64894
idatasubcom-oss <idatasub.com@gmail.com>
2026-05-26 23:02:09 +0100

    Update userController.js

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,5 +1,29 @@
-exports.getUser = (req, res) => {
-  res.json({
-    name: "IDATASUBFAST User"
-  });
+const User = require("../models/User");
+
+const registerUser = async (req, res) => {
+  try {
+    const { name, email, password } = req.body;
+
+    const user = new User({
+      name,
+      email,
+      password
+    });
+
+    await user.save();
+
+    res.status(201).json({
+      message: "User registered successfully",
+      user
+    });
+
+  } catch (error) {
+    res.status(500).json({
+      message: error.message
+    });
+  }
+};
+
+module.exports = {
+  registerUser
 };
