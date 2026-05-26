c6aa0818fd3427f5fb8f79e2fd801da4863112b4
idatasubcom-oss <idatasub.com@gmail.com>
2026-05-26 22:54:18 +0100

    Remove user routes and express.json middleware
    Removed user routes and JSON parsing middleware from server.

--- server.js
+++ server.js
@@ -19,7 +19,3 @@ const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
-app.use(express.json());
-const userRoutes = require("./routes/userRoutes");
-
-app.use("/api/users", userRoutes);
